80d928701207fb5b4a7aa53776e41403cf121cc5
Jamal <jamal.degeratto@gmail.com>
2022-11-16 07:11:23 -0600

    Unmonolith attendees

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,13 +1,17 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee
-from events.models import Conference
+from .models import Attendee, ConferenceVO
 from django.views.decorators.http import require_http_methods
 import json
 
 
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
+
 class ConferenceNameEncoder(ModelEncoder):
-    model = Conference
+    model = ConferenceVODetailEncoder
     properties = ["name"]
 
 
@@ -33,7 +37,7 @@ class AttendeeListEncoder(ModelEncoder):
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -54,7 +58,7 @@ def api_list_attendees(request, conference_id):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             attendees,
             encoder=AttendeeListEncoder,
@@ -64,9 +68,10 @@ def api_list_attendees(request, conference_id):
         content = json.loads(request.body)
 
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f'/api/conferences/{conference_vo_id}/'
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400
@@ -118,9 +123,9 @@ def api_show_attendee(request, id):
 
         try:
             if "conference" in content:
-                conference = Conference.objects.get(id=content["conference"])
+                conference = ConferenceVO.objects.get(id=content["conference"])
                 content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400
